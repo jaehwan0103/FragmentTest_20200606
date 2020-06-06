f1a6f7729505aca0941cd7dbd6b81a636278e387
jaehwan0103 <dkdlfkqb1@gmail.com>
2020-06-06 13:36:52 +0900

    Initial commit

---
+++ app/src/main/java/kr/ptus/fragmenttest_20200606/MainActivity.kt
@@ -0,0 +1,23 @@
+package kr.ptus.fragmenttest_20200606
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupEvents()
+        setValuse()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValuse() {
+
+    }
+}
